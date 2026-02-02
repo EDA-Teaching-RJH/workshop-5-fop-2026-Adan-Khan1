256127cc49d120ba2a8b05274e3744427ea5a78b
Adan-Khan1 <ak139@kent.ac.uk>
2026-02-02 13:24:22 +0000

    .

--- camel.py
+++ camel.py
@@ -4,18 +4,6 @@ print("snake_case: ", end="")
 
 for char in camel_case:
     if char.isupper():
-        # Print an underscore and the lowercase version of the letter
-        print("_" + char.lower(), end="")
-    else:
-        print(char, end="")
-
-mcamel_case = input("camelCase: ")
-
-print("snake_case: ", end="")
-
-for char in camel_case:
-    if char.isupper():
-        # Print an underscore and the lowercase version of the letter
         print("_" + char.lower(), end="")
     else:
         print(char, end="")
